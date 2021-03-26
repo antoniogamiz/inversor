923e6e01b9fdf7b4d9d1a5b19bda8a2ecb5146f0
Antonio <antoniogamiz10@gmail.com>
2021-03-26 20:58:12 +0100

    deployment(fix): parse port correctly

--- server.ts
+++ server.ts
@@ -2,10 +2,12 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 
 const app = new Application();
 
-const PORT = ((Deno.env.get("PORT") as unknown) as number) || 8000;
+const PORT = parseInt((Deno.env.get("PORT") as unknown) as string) || 8000;
 
 app.use((ctx) => {
   ctx.response.body = "Hello World!";
 });
+
+console.log(PORT);
 
 await app.listen({ port: PORT });

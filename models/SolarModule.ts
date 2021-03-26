e6ed11cc59e829f8f02602238edc670b804fa081
Antonio <antoniogamiz10@gmail.com>
2021-03-26 20:03:05 +0100

    feat: basic server

---
+++ models/SolarModule.ts
@@ -0,0 +1,17 @@
+export interface SolarModule {
+  brand: string;
+  model: string;
+  description: string;
+  ratedPower: string;
+  voc: bigint;
+  isc: bigint;
+  vmp: bigint;
+  imp: bigint;
+  efficiency: number;
+  cvoc: bigint;
+  cisc: bigint;
+  provider: string;
+  discount: number;
+  pvp: number;
+  price: number;
+}

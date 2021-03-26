0b69bc7122f5a1628a32b4be1a51809c6c104781
Antonio <antoniogamiz10@gmail.com>
2021-03-26 20:35:52 +0100

    deployment: read port from environment

--- server.ts
+++ server.ts
@@ -2,8 +2,10 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 
 const app = new Application();
 
+const PORT = ((Deno.env.get("PORT") as unknown) as number) || 8000;
+
 app.use((ctx) => {
   ctx.response.body = "Hello World!";
 });
 
-await app.listen({ port: 8000 });
+await app.listen({ port: PORT });

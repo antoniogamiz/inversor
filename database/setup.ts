5e74b7ae390f6cb349f16adf34cb021aaf11c645
Antonio <antoniogamiz10@gmail.com>
2021-03-29 14:20:28 +0200

    build: move config to .env file

--- database/setup.ts
+++ database/setup.ts
@@ -1,20 +1,18 @@
 import { PostgresConnector, Database } from "denodb/mod.ts";
 import { InversorModel } from "./mod.ts";
 
-const MODELS = [InversorModel];
-
 export const setupDataBase = async () => {
   const connection = new PostgresConnector({
-    host: "localhost",
-    username: "postgres",
-    password: "antonio",
-    database: "inversor",
-    port: 5432,
+    host: Deno.env.get("DB_HOST") || "",
+    username: Deno.env.get("DB_USERNAME") || "",
+    password: Deno.env.get("DB_PASSWORD") || "",
+    database: Deno.env.get("DB_NAME") || "",
+    port: parseInt((Deno.env.get("DB_PORT") as unknown) as string) || 5432,
   });
 
   const db = new Database(connection);
 
-  db.link(MODELS);
+  db.link([InversorModel]);
   await db.sync();
 
   return db;

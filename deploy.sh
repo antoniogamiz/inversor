5efb4cf6b90feae407f56f9010a5dca6479eed94
Antonio <antoniogamiz10@gmail.com>
2021-03-31 19:25:45 +0200

    fix(deployment): reorder commands to make deploy.sh available

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
-PID=`cat save_pid.txt`
-kill -9 $PID || true
 rm -rf prod
 mkdir -p prod
-cd prod
 git clone https://github.com/antoniogamiz/inversor.git .
+cd prod
+PID=`cat ../save_pid.txt`
+kill -9 $PID || true
 nohup vr prod > ../my.log &
 echo $! > ../save_pid.txt
 echo "Deployment successful!"

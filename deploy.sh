aee685fa40f7d9699ba441ed11ff06d7909225bc
Antonio <antoniogamiz10@gmail.com>
2021-03-31 19:34:26 +0200

    fix(deployment): nohup running as background

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 PID=`cat ../save_pid.txt`
 kill -9 $PID || true
 cp ../.env .env
-nohup vr prod > ../my.log
+nohup vr prod > ../my.log &
 cat ../my.log
 echo $! > ../save_pid.txt
 echo "Deployment successful!"
